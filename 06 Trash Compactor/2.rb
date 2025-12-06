1382c391b989e386386cc04799c597a1903f7ca4
ParadoxV5 <paradox.ver5@gmail.com>
2025-12-06 00:06:08 -0700

    06.2

---
+++ 06 Trash Compactor/2.rb
@@ -0,0 +1,11 @@
+# Tip: both `+` and `-` are commutative (order doesn’t matter).
+
+puts(ARGF.each_line.map(&:chars)
+  .transpose #: Array[Array[String]]
+  .each { it.delete ' ' }
+  .slice_after(&:empty?)
+  .sum do|*operands, _column_delimiter|
+    operator = operands.first.pop #: String
+    operands.map { Integer(it.join) }.inject operator.to_sym
+  end
+)
