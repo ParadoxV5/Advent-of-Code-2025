cdfe18e40ef0d6b01a0e9090dfd7f8e5a9ac4754
ParadoxV5 <paradox.ver5@gmail.com>
2025-12-22 21:53:35 -0700

    12.🎨

---
+++ 12 Christmas Tree Farm/1.art.rb
@@ -0,0 +1,16 @@
+# …
+# 
+# 5:
+i=ARGF.readlines'';p(i
+.last.lines.count{w,h,
+        *q=*it
+        .scan(
+        /\d+/)
+        .map(&
+        :to_i)
+        q.each
+.with_index.sum{;i[_2]
+.count('#')*_1}<=w*h})
+
+# 22x10: 0 0 0 0 0 1
+# 67x69: 0 0 0 0 0 42
